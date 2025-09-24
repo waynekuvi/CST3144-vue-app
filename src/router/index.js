58ddb258790cc6c5bd5a5c5c7389eeb6b536e969
18aint <waynekuvi@gmail.com>
2025-09-24 06:34:10 +0100

    fix: router configuration for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const LessonsPage = () => import('../views/LessonsPage.vue')
 const CartPage = () => import('../views/CartPage.vue')
 
 const router = createRouter({
-	history: createWebHistory('/CST3144-vue-app/'),
+	history: createWebHashHistory(),
 	routes: [
 		{ path: '/', name: 'Lessons', component: LessonsPage },
 		{ path: '/cart', name: 'Cart', component: CartPage },
