842bc5b698be584b87776936a4e2be757baecefb
18aint <waynekuvi@gmail.com>
2025-09-24 06:29:59 +0100

    fix: simplify GitHub Pages deployment with hash routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const LessonsPage = () => import('../views/LessonsPage.vue')
 const CartPage = () => import('../views/CartPage.vue')
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory('/CST3144-vue-app/'),
 	routes: [
 		{ path: '/', name: 'Lessons', component: LessonsPage },
 		{ path: '/cart', name: 'Cart', component: CartPage },
